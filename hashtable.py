5c0bbeb47308937be601b98075bbcf615cd97bf4
Dantun1 <daniel@tunyan.com>
2025-07-22 00:42:43 +0200

    Implement HashTable class with key API

--- hashtable.py
+++ hashtable.py
@@ -1,13 +1,66 @@
+from typing import NamedTuple
+
+
+class Pair(NamedTuple):
+    key: str
+    value: str
+
+
 class HashTable:
+
+    @classmethod
+    def from_dict(cls, pairs, size=10):
+        if size < len(pairs):
+            raise ValueError(f"size: {size} must be greater than or equal to the number of pairs: {len(pairs)}")
+        ht = cls(size)
+        for key, value in pairs.items():
+            ht[key] = value
+        return ht
+
+
     def __init__(self, size):
-        self.size = size
-        self.values = [None] * size
+        if size < 1:
+            raise ValueError("size must be greater than 0")
+        self._slots = [None] * size
+
+    @property
+    def pairs(self):
+        return {pair for pair in self._slots if pair}
+
+    @property
+    def keys(self):
+        return {key for key, _ in self.pairs}
+
+    @property
+    def values(self):
+        return [value for _, value in self.pairs]
+
+    @property
+    def size(self):
+        return len(self._slots)
+
+    def _get_index(self, key):
+        return hash(key) % self.size
 
     def __len__(self):
-        return self.size
+        return len(self.pairs)
 
     def __setitem__(self, key, value):
-        index = hash(key) % len(self)
-        self.values[index] = value
+        self._slots[self._get_index(key)] = Pair(key, value)
 
+    def __getitem__(self, key):
+        if pair := self._slots[self._get_index(key)] :
+            return pair.value
+        else:
+            raise KeyError("key not found")
 
+    def __str__(self):
+        return "{" + str(", ".join([f"{key!r}:{value!r}"for key,value in self.pairs])) +"}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({str(self)})"
+
+    def __eq__(self, other):
+        return self.pairs == other.pairs
+
+# TODO: Add other nonessential dict methods like copy,get
--- test_hashtable.py
+++ test_hashtable.py
@@ -1,23 +1,106 @@
-from hashtable import HashTable
+from hashtable import HashTable, Pair
+import pytest
+from pytest_unordered import unordered
 
+# Initialisation
 
 def test_should_create_hash_table():
     assert HashTable(size=50) is not None
 
-def test_should_have_size():
-    assert len(HashTable(size=50)) == 50
+def test_should_create_pair():
+    assert Pair(key="key", value="value") is not None
 
-def test_should_have_none_values_on_initialisation():
-    ht = HashTable(size=10)
-    nones = [None for _ in range(10)]
-    assert ht.values == nones
+def test_should_fill_with_nones_on_init():
+    assert HashTable(size=50)._slots == [None for _ in range(50)]
 
-def test_should_insert_value():
+# Setting and getting pairs
+
+@pytest.fixture(scope="module")
+def hash_table():
     ht = HashTable(size=10)
-    ht["John"] = "Smith"
-    ht[52] = "Blah"
-    ht["A"] = True
-    assert "Smith" in ht.values
-    assert "Blah" in ht.values
-    assert True in ht.values
-    assert len(ht.values) == len(ht)
+    ht["hello"] = "world"
+    ht[17] = 18
+    ht[True] = False
+    return ht
+
+def test_should_insert_specific_pairs(hash_table):
+
+    assert ("hello","world") in hash_table.pairs
+    assert (17,18) in hash_table.pairs
+    assert (True,False) in hash_table.pairs
+
+def test_shoud_get_length_of_pairs(hash_table):
+    assert len(hash_table) == 3
+
+def test_should_get_specific_inserted_values(hash_table):
+    assert hash_table["hello"] == "world"
+    assert hash_table[17] == 18
+    assert hash_table[True] == False
+
+def test_should_raise_key_error_on_invalid_key():
+    with pytest.raises(KeyError) as exception:
+        ht = HashTable(size=10)["invalid_key"]
+
+    assert exception.type == KeyError
+    assert str(exception.value) == "'key not found'"
+
+def test_should_get_all_keys(hash_table):
+    assert hash_table.keys == {"hello",17,True}
+
+def test_should_get_empty_keys_on_empty_hash_table():
+    assert HashTable(10).keys == set()
+
+def test_should_get_all_values(hash_table):
+    assert unordered(hash_table.values) == ["world",18,False]
+
+def test_should_get_empty_values_on_empty_hash_table():
+    assert HashTable(10).values == []
+
+def test_should_create_copies_of_data(hash_table):
+    assert hash_table.pairs is not hash_table.pairs
+    assert hash_table.keys is not hash_table.keys
+    assert hash_table.values is not hash_table.values
+
+def test_should_not_create_hashtable_with_non_positive_size():
+    with pytest.raises(ValueError) as exception:
+        HashTable(size=0)
+
+    assert exception.type == ValueError
+
+def test_should_report_size(hash_table):
+    assert hash_table.size == 10
+
+# Additional utility functionality
+
+def test_should_print_pairs(hash_table):
+    assert str(hash_table) in {"{'hello':'world', 17:18, True:False}",
+                               "{17:18, 'hello':'world', True:False}",
+                               "{True:False, 17:18, 'hello':'world'}",
+                               "{'hello':'world', True:False, 17:18}",
+                               "{17:18, 'hello':'world', True:False}",
+                               "{True:False, 'hello':'world', 17:18}",
+                               }
+
+def test_should_make_from_dict(hash_table):
+    assert HashTable.from_dict(dict(hash_table.pairs)) == hash_table
+
+def test_should_make_from_dict_with_custom_size(hash_table):
+    assert HashTable.from_dict(dict(hash_table.pairs), size = 10)
+
+def test_should_raise_value_error_from_dict_with_custom_size_lower_than_size(hash_table):
+    with pytest.raises(ValueError) as exception:
+        HashTable.from_dict(dict(hash_table.pairs), size = 2)
+
+    assert exception.type == ValueError
+
+def test_should_repr_hash_table(hash_table):
+    assert repr(hash_table) in {"HashTable({'hello':'world', 17:18, True:False})",
+                                "HashTable({'hello':'world', True:False, 17:18})",
+                                "HashTable({17:18, 'hello':'world', True:False})",
+                                "HashTable({17:18, True:False, 'hello':'world'})",
+                                "HashTable({True:False, 'hello':'world, 17:18})",
+                                "HashTable({True:False, 17:18, 'hello':'world'})",
+                                }
+
+# TODO: More tests for equality
+
